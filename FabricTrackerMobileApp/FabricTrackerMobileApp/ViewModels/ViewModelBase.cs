039e5054627bfafc6aa1d0c269a2eb06221eaa2f
Katherine Hambley <superdesigngirl@mac.com>
2022-08-24 15:39:03 -0500

    Added ViewModelBase

---
+++ FabricTrackerMobileApp/FabricTrackerMobileApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FabricTrackerMobileApp.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(params string[] propertyNames)
+        {
+            foreach (var propertyName in propertyNames)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public INavigation Navigation { get; set; }
+    }
+}
