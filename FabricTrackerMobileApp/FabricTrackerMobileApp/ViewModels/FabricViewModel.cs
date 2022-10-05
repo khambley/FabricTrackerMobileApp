f75ad0261a94a72445e97d32692139d930af8078
Katherine Hambley <superdesigngirl@mac.com>
2022-10-05 10:02:05 -0500

    Image thumbnail displaying in Fabrics Listview

--- FabricTrackerMobileApp/FabricTrackerMobileApp/ViewModels/FabricViewModel.cs
+++ FabricTrackerMobileApp/FabricTrackerMobileApp/ViewModels/FabricViewModel.cs
@@ -1,18 +1,46 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using FabricTrackerMobileApp.Models;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace FabricTrackerMobileApp.ViewModels
 {
     public class FabricViewModel : ViewModelBase
     {
-        public FabricViewModel(Fabric item)
-        {
-            Item = item;
-        }
+        public Fabric Item { get; private set; }
+
+        public ImageSource ImageSource { get; set; }
+
+        public byte[] ImageBytes { get; set; }
 
         public event EventHandler ItemStatusChanged;
 
-        public Fabric Item { get; private set; }
+        public FabricViewModel(Fabric item)
+        {
+            Item = item;
+            ImageSource = LoadPhoto();
+        }
+
+        private ImageSource LoadPhoto()
+        {
+            //retrieve the image from ImagePath in db.
+            var filePath = Item.ImagePath;
+            if (File.Exists(filePath))
+            {
+                ImageBytes = File.ReadAllBytes(filePath);
+                ImageSource = ImageSource.FromStream(() => new MemoryStream(ImageBytes));
+                return ImageSource;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        
     }
 }
 
