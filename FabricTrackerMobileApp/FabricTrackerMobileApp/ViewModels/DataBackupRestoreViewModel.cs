9d7c1521909edba474c1561da9a0c213664f0b19
Katherine Hambley <superdesigngirl@mac.com>
2022-11-04 12:10:09 -0500

    Added Settings and BackupRestore Pages

---
+++ FabricTrackerMobileApp/FabricTrackerMobileApp/ViewModels/DataBackupRestoreViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FabricTrackerMobileApp.Data;
+
+namespace FabricTrackerMobileApp.ViewModels
+{
+    public class DataBackupRestoreViewModel : ViewModelBase
+    {
+        private readonly Repository repository;
+
+        public DataBackupRestoreViewModel(Repository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
+
---
+++ FabricTrackerMobileApp/FabricTrackerMobileApp/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FabricTrackerMobileApp.Data;
+using FabricTrackerMobileApp.Pages;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FabricTrackerMobileApp.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        private readonly Repository repository;
+
+        public SettingsViewModel(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ICommand NavigateToDataBackupRestorePage => new Command(async () =>
+        {
+            var dataBackupRestorePage = Resolver.Resolve<DataBackupRestorePage>();
+            await Navigation.PushAsync(dataBackupRestorePage);
+        });
+
+    }
+}
+
